825a997c6b94ab566720fa981f7cdcaef2b2f656
tana.suksangrat <tana.suksangrat@accenture.com>
2018-08-28 13:26:35 +0700

    update

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     client.user.setActivity('Ragnarok', {type: 'WATCHING'});
-	const vChannel = client.channels.get("483838057498542083");
-	const tChannel = client.channels.get("483838057498542081");
-  if (!vChannel) return tChannel.send("Voice channel is not found");
-  channel.join().then(connection => {
-    // Yay, it worked!
-    console.log("Successfully connected.");
-	tChannel.send("Voice connected")
-  }).catch(e => {
-    // Oh no, it errored! Let's log it to console :)
-    console.error(e);
-  }
+	// const vChannel = client.channels.get("483838057498542083");
+	// const tChannel = client.channels.get("483838057498542081");
+  // if (!vChannel) return tChannel.send("Voice channel is not found");
+  // channel.join().then(connection => {
+    //Yay, it worked!
+    // console.log("Successfully connected.");
+	// tChannel.send("Voice connected")
+  // }).catch(e => {
+    //Oh no, it errored! Let's log it to console :)
+    // console.error(e);
+  // }
   });
 	// const channel = member.guild.channels.find(ch => ch.name === 'general');
 	// channel.send('Hello, I am online');
