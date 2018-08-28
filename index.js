989547ebb436bb4743ed61b884c7f19af5a08e3d
tana.suksangrat <tana.suksangrat@accenture.com>
2018-08-28 14:51:29 +0700

    make emo

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     client.user.setActivity('https://git.io/d.js-heroku', {type: 'WATCHING'});
-	try{
-		client.channels.get('315373134670725120').send('I am online. see *help for command');
-	}
-	catch(e){
-	}
+	// try{
+		// client.channels.get('315373134670725120').send('I am online. see *help for command');
+	// }
+	// catch(e){
+	// }
 	try{
 		client.channels.get('483838057498542081').send('I am online. see *help for command');
 	}
@@ -40,8 +40,24 @@ client.on('message', msg => {
 		msg.reply("Here is your search list for "+item[1]);
 		msg.reply("http://ratemyserver.net/index.php?iname="+item[1]+"&page=re_item_db&quick=1&isearch=Search");
 	}else{
-		msg.reply("lenght = "+mon.length);
+		msg.reply("length = "+mon.length);
 	}
   }
+   if (msg.content.toLowerCase().startsWith("*createmo")) {
+	    var emo = msg.content.toLowerCase().split(" ");
+		if(emo.length === 3){
+			var fs = require('fs'),
+			request = require('request');
+
+			var download = function(uri, filename, callback){
+				request.head(uri, function(err, res, body){
+				console.log('content-type:', res.headers['content-type']);
+				console.log('content-length:', res.headers['content-length']);
+				request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+			  });
+			};
+			msg.reply("in 3");
+		}
+   }
 });
 client.login('NDgzODMzMDUxOTg2NTI2MjA4.DmZMuw.Q77zRpCY-85EdyzzhUMzqA1B88Y');
